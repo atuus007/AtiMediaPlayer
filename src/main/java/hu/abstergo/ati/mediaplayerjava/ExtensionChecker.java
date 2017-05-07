57d6d9c0864b9a2447b099da2cc2a1f5bccbd7d1
atuus007 <fodor.attila14@gmail.com>
2017-05-07 23:33:17 +0200

    Extension filter implementation

--- src/main/java/hu/abstergo/ati/mediaplayerjava/ExtensionChecker.java
+++ src/main/java/hu/abstergo/ati/mediaplayerjava/ExtensionChecker.java
@@ -10,5 +10,18 @@ package hu.abstergo.ati.mediaplayerjava;
  * @author Ati
  */
 public class ExtensionChecker {
-    
+    public static boolean isGoodExtension(String path){
+        String temp;
+        int pos = 0;
+        if (path.lastIndexOf(".") != -1) {
+            pos = path.lastIndexOf(".");
+        }
+        temp = path.substring(pos + 1, path.length());
+        if(temp.equals("mp3")|| temp.equals("mp4")){
+            return true;
+        }else {
+            return false;
+        }
+        
+    }
 }
